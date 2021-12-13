c20122ffc5809fcb2cd3d9d8d68d929e21e8456f
Antonio Merino Gallardo <32602077+amerigal@users.noreply.github.com>
2021-12-13 10:17:46 +0100

    Arregla generación de revisores. Closes #314. (#315)
    * Arregla generación de revisores. Closes #314.
    
    * Elimina dependencia (#315). Closes #314

--- src/random-reviewer.pl
+++ src/random-reviewer.pl
@@ -8,7 +8,7 @@ use JSON;
 use File::Slurper 'read_text';
 use GitHub::Actions;
 
-use constant REVIEWERS => 3;
+use constant MAXREVIEWERS => 3;
 
 my $objetivos_json = read_text( "data/objetivos.json" ) || die "No encuentro el fichero de objetivos";
 
@@ -18,8 +18,9 @@ my $este_objetivo = $ENV{'objetivo'};
 
 my @reviewers;
 my @these_students = @{$objetivos[$este_objetivo]};
+my $num_reviewers = scalar(@these_students) < MAXREVIEWERS ? scalar(@these_students) : MAXREVIEWERS;
 
-for ( my $i = 0; $i < REVIEWERS; $i ++ ) {
+for ( my $i = 0; $i < $num_reviewers; $i ++ ) {
   my $this_reviewer = splice( @these_students, int(rand( $#these_students ) ), 1 );
   push( @reviewers, "\@".$this_reviewer );
 }
